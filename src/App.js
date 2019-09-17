458986202f0da0523128aa9298ee50c1778253fa
alexpstein <alexstein47@gmail.com>
2019-09-17 18:57:46 -0400

    fetch items from order, display

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import './App.css';
+import Item from './components/Item/Item';
+import './App.scss';
 
 class App extends Component {
   state = {
-    data: {}
+    data: {},
+    error: false
   }
 
   componentDidMount() {
     axios.get('https://gopuff-public.s3.amazonaws.com/dev-assignments/product/order.json')
       .then(response => {
         this.setState({data: response.data})
+      })
+      .catch(error =>  {
+        this.setState({error: true})
       });
   }
 
   render() {
+    let products = <p>Something went wrong.</p>;
+    if (this.state.data.cart === undefined) {
+      products = <p>Loading...</p>;
+    } else if (!this.state.error) {
+      products = this.state.data.cart.products.map(product => {
+        return <Item key={product.id} id={product.id} />
+      });
+      
+    }
+
     return (
       <div className="App">
-        
+        <section className="Products">
+          {products}
+        </section>
       </div>
     );
   }
