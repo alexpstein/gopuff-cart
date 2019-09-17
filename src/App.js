e536220d4b7fe2942faf6ba03fe9be0ade826dbb
alexpstein <alexstein47@gmail.com>
2019-09-17 16:24:45 -0400

    install and initialize state with axios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: {}
+  }
+
+  componentDidMount() {
+    axios.get('https://gopuff-public.s3.amazonaws.com/dev-assignments/product/order.json')
+      .then(response => {
+        this.setState({data: response.data})
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
 }
 
 export default App;
